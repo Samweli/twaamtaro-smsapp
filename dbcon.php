04b0b861d2df1c18afeef01b77faaabf460bf537
Samweli <smwltwesa6@gmail.com>
2017-08-16 15:28:39 +0300

    updated database credentials

--- dbcon.php
+++ dbcon.php
@@ -1,6 +1,6 @@
 <?php
 
-$dbcon = pg_connect("host=127.0.0.1 port=5432 dbname=mitaro user=postgres password=nancy");
+$dbcon = pg_connect("host=127.0.0.1 port=5432 dbname=smstest user=smsuser password=workhard");
 
   //For Checking if the connection has been established
    /*if(!$dbcon) {
